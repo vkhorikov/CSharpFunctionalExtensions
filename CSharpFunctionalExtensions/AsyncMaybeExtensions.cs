4e6a3dcadce431592b31b1b0c6fccfa6f6a472f8
vkhorikov <vladimir.khorikov@gmail.com>
2016-07-22 16:58:27 -0400

    Async maybe extension

---
+++ CSharpFunctionalExtensions/AsyncMaybeExtensions.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+
+
+namespace CSharpFunctionalExtensions
+{
+    public static class AsyncMaybeExtensions
+    {
+        public static async Task<Result<T>> ToResult<T>(this Task<Maybe<T>> maybeTask, string errorMessage)
+            where T : class
+        {
+            Maybe<T> maybe = await maybeTask;
+            return maybe.ToResult(errorMessage);
+        }
+    }
+}
