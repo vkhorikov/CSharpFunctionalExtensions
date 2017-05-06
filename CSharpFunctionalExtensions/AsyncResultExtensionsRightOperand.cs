5a151d7338cb8eab85e13f11da103e9c840dca18
Ivan Novikov <jonny.novikov@gmail.com>
2017-05-06 18:23:17 +0300

    Added continueOnCapturedContext parameter to Async extensions (#30)
    * Revert "Update to netstandard 1.3, added ISerializable"
    
    * Added continueOnCapturedContext parameter to Async extensions

--- CSharpFunctionalExtensions/AsyncResultExtensionsRightOperand.cs
+++ CSharpFunctionalExtensions/AsyncResultExtensionsRightOperand.cs
@@ -8,163 +8,163 @@ namespace CSharpFunctionalExtensions
     /// </summary>
     public static class AsyncResultExtensionsRightOperand
     {
-        public static async Task<Result<K>> OnSuccess<T, K>(this Result<T> result, Func<T, Task<K>> func)
+        public static async Task<Result<K>> OnSuccess<T, K>(this Result<T> result, Func<T, Task<K>> func, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
                 return Result.Fail<K>(result.Error);
 
-            K value = await func(result.Value).ConfigureAwait(false);
+            K value = await func(result.Value).ConfigureAwait(continueOnCapturedContext);
 
             return Result.Ok(value);
         }
 
-        public static async Task<Result<T>> OnSuccess<T>(this Result result, Func<Task<T>> func)
+        public static async Task<Result<T>> OnSuccess<T>(this Result result, Func<Task<T>> func, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
                 return Result.Fail<T>(result.Error);
 
-            T value = await func().ConfigureAwait(false);
+            T value = await func().ConfigureAwait(continueOnCapturedContext);
 
             return Result.Ok(value);
         }
 
-        public static async Task<Result<K>> OnSuccess<T, K>(this Result<T> result, Func<T, Task<Result<K>>> func)
+        public static async Task<Result<K>> OnSuccess<T, K>(this Result<T> result, Func<T, Task<Result<K>>> func, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
                 return Result.Fail<K>(result.Error);
 
-            return await func(result.Value).ConfigureAwait(false);
+            return await func(result.Value).ConfigureAwait(continueOnCapturedContext);
         }
 
-        public static async Task<Result<T>> OnSuccess<T>(this Result result, Func<Task<Result<T>>> func)
+        public static async Task<Result<T>> OnSuccess<T>(this Result result, Func<Task<Result<T>>> func, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
                 return Result.Fail<T>(result.Error);
 
-            return await func().ConfigureAwait(false);
+            return await func().ConfigureAwait(continueOnCapturedContext);
         }
 
-        public static async Task<Result<K>> OnSuccess<T, K>(this Result<T> result, Func<Task<Result<K>>> func)
+        public static async Task<Result<K>> OnSuccess<T, K>(this Result<T> result, Func<Task<Result<K>>> func, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
                 return Result.Fail<K>(result.Error);
 
-            return await func().ConfigureAwait(false);
+            return await func().ConfigureAwait(continueOnCapturedContext);
         }
 
-        public static async Task<Result> OnSuccess<T>(this Result<T> result, Func<T, Task<Result>> func)
+        public static async Task<Result> OnSuccess<T>(this Result<T> result, Func<T, Task<Result>> func, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
                 return Result.Fail(result.Error);
 
-            return await func(result.Value).ConfigureAwait(false);
+            return await func(result.Value).ConfigureAwait(continueOnCapturedContext);
         }
 
-        public static async Task<Result> OnSuccess(this Result result, Func<Task<Result>> func)
+        public static async Task<Result> OnSuccess(this Result result, Func<Task<Result>> func, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
                 return result;
 
-            return await func().ConfigureAwait(false);
+            return await func().ConfigureAwait(continueOnCapturedContext);
         }
 
-        public static async Task<Result<T>> Ensure<T>(this Result<T> result, Func<T, Task<bool>> predicate, string errorMessage)
+        public static async Task<Result<T>> Ensure<T>(this Result<T> result, Func<T, Task<bool>> predicate, string errorMessage, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
                 return Result.Fail<T>(result.Error);
 
-            if (!await predicate(result.Value).ConfigureAwait(false))
+            if (!await predicate(result.Value).ConfigureAwait(continueOnCapturedContext))
                 return Result.Fail<T>(errorMessage);
 
             return Result.Ok(result.Value);
         }
 
-        public static async Task<Result> Ensure(this Result result, Func<Task<bool>> predicate, string errorMessage)
+        public static async Task<Result> Ensure(this Result result, Func<Task<bool>> predicate, string errorMessage, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
                 return Result.Fail(result.Error);
 
-            if (!await predicate().ConfigureAwait(false))
+            if (!await predicate().ConfigureAwait(continueOnCapturedContext))
                 return Result.Fail(errorMessage);
 
             return Result.Ok();
         }
 
-        public static async Task<Result<K>> Map<T, K>(this Result<T> result, Func<T, Task<K>> func)
+        public static async Task<Result<K>> Map<T, K>(this Result<T> result, Func<T, Task<K>> func, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
                 return Result.Fail<K>(result.Error);
 
-            K value = await func(result.Value).ConfigureAwait(false);
+            K value = await func(result.Value).ConfigureAwait(continueOnCapturedContext);
 
             return Result.Ok(value);
         }
 
-        public static async Task<Result<T>> Map<T>(this Result result, Func<Task<T>> func)
+        public static async Task<Result<T>> Map<T>(this Result result, Func<Task<T>> func, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
                 return Result.Fail<T>(result.Error);
 
-            T value = await func().ConfigureAwait(false);
+            T value = await func().ConfigureAwait(continueOnCapturedContext);
 
             return Result.Ok(value);
         }
 
-        public static async Task<Result<T>> OnSuccess<T>(this Result<T> result, Func<T, Task> action)
+        public static async Task<Result<T>> OnSuccess<T>(this Result<T> result, Func<T, Task> action, bool continueOnCapturedContext = false)
         {
             if (result.IsSuccess)
             {
-                await action(result.Value).ConfigureAwait(false);
+                await action(result.Value).ConfigureAwait(continueOnCapturedContext);
             }
 
             return result;
         }
 
-        public static async Task<Result> OnSuccess(this Result result, Func<Task> action)
+        public static async Task<Result> OnSuccess(this Result result, Func<Task> action, bool continueOnCapturedContext = false)
         {
             if (result.IsSuccess)
             {
-                await action().ConfigureAwait(false);
+                await action().ConfigureAwait(continueOnCapturedContext);
             }
 
             return result;
         }
 
-        public static async Task<T> OnBoth<T>(this Result result, Func<Result, Task<T>> func)
+        public static async Task<T> OnBoth<T>(this Result result, Func<Result, Task<T>> func, bool continueOnCapturedContext = false)
         {
-            return await func(result).ConfigureAwait(false);
+            return await func(result).ConfigureAwait(continueOnCapturedContext);
         }
 
-        public static async Task<K> OnBoth<T, K>(this Result<T> result, Func<Result<T>, Task<K>> func)
+        public static async Task<K> OnBoth<T, K>(this Result<T> result, Func<Result<T>, Task<K>> func, bool continueOnCapturedContext = false)
         {
-            return await func(result).ConfigureAwait(false);
+            return await func(result).ConfigureAwait(continueOnCapturedContext);
         }
 
-        public static async Task<Result<T>> OnFailure<T>(this Result<T> result, Func<Task> func)
+        public static async Task<Result<T>> OnFailure<T>(this Result<T> result, Func<Task> func, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
             {
-                await func().ConfigureAwait(false);
+                await func().ConfigureAwait(continueOnCapturedContext);
             }
 
             return result;
         }
 
-        public static async Task<Result> OnFailure(this Result result, Func<Task> func)
+        public static async Task<Result> OnFailure(this Result result, Func<Task> func, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
             {
-                await func().ConfigureAwait(false);
+                await func().ConfigureAwait(continueOnCapturedContext);
             }
 
             return result;
         }
 
-        public static async Task<Result<T>> OnFailure<T>(this Result<T> result, Func<string, Task> func)
+        public static async Task<Result<T>> OnFailure<T>(this Result<T> result, Func<string, Task> func, bool continueOnCapturedContext = false)
         {
             if (result.IsFailure)
             {
-                await func(result.Error).ConfigureAwait(false);
+                await func(result.Error).ConfigureAwait(continueOnCapturedContext);
             }
 
             return result;
