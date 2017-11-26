6df274a0b9caef32ea342dbbcb60ed1fb6fdf763
mjharwood <mjh-github@liminalfllux.net>
2017-11-25 19:49:39 -0500

    multiframework support:
    * default to Standard 2.0
    * build netstandard2.0;netstandard1.3;net40;net45;net461
    * added framework specific references where necessary
    * labeled per-framework dll with which framework it uses
    * added per-framework link tests to verify the package works with each version

---
+++ LinkTest.Standard.2.0/Test.cs
@@ -0,0 +1,14 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace LinkTest
+{
+    class Test
+    {
+        static void ForceLink() // If the library isn't referenced, it doesn't actually get linked
+        {
+            Maybe<Test> maybe = null;
+
+            System.Diagnostics.Debug.WriteLine(string.Format("maybe HasValue: {0} hasNoValue {1}", maybe.HasValue, maybe.HasNoValue));
+        }
+    }
+}
