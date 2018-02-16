41811fa820919c9060a142c7346d5e397f11ca6a
Vladimir <vladimir.khorikov@gmail.com>
2018-02-16 15:30:03 -0500

    Non-generic Value Object

---
+++ CSharpFunctionalExtensions.Tests/ValueObjectTests/BasicTests.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpFunctionalExtensions.Tests.ValueObjectTests
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void Derived_value_objects_dont_match()
+        {
+            var address = new Address("Street", "City");
+            var derivedAddress = new DerivedAddress("Country", "Street", "City");
+
+            address.Equals(derivedAddress).Should().BeFalse();
+            derivedAddress.Equals(address).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Two_VO_of_the_same_content_are_equal()
+        {
+            var address1 = new Address("Street", "City");
+            var address2 = new Address("Street", "City");
+
+            address1.Equals(address2).Should().BeTrue();
+            address1.GetHashCode().Equals(address2.GetHashCode()).Should().BeTrue();
+        }
+
+        [Fact]
+        public void It_is_possible_to_override_defaul_equality_comparison_behavior()
+        {
+            var money1 = new Money("usd", 2.2222m);
+            var money2 = new Money("USD", 2.22m);
+
+            money1.Equals(money2).Should().BeTrue();
+            money1.GetHashCode().Equals(money2.GetHashCode()).Should().BeTrue();
+        }
+
+        public class Money : ValueObject
+        {
+            public string Currency { get; }
+            public decimal Amount { get; }
+
+            public Money(string currency, decimal amount)
+            {
+                Currency = currency;
+                Amount = amount;
+            }
+
+            protected override IEnumerable<object> GetEqualityComponents()
+            {
+                yield return Currency.ToUpper();
+                yield return Math.Round(Amount, 2);
+            }
+        }
+
+        public class Address2 : ValueObject
+        {
+            public string Street { get; }
+            public string City { get; }
+
+            public Address2(string street, string city)
+            {
+                Street = street;
+                City = city;
+            }
+
+            protected override IEnumerable<object> GetEqualityComponents()
+            {
+                yield return Street;
+                yield return City;
+            }
+        }
+
+        public class Address : ValueObject<Address>
+        {
+            public string Street { get; }
+            public string City { get; }
+
+            public Address(string street, string city)
+            {
+                Street = street;
+                City = city;
+            }
+
+            protected override bool EqualsCore(Address other)
+            {
+                return Street == other.Street && City == other.City;
+            }
+
+            protected override int GetHashCodeCore()
+            {
+                unchecked
+                {
+                    int hashCode = Street.GetHashCode();
+                    hashCode = (hashCode * 397) ^ City.GetHashCode();
+                    return hashCode;
+                }
+            }
+        }
+
+        public class DerivedAddress : Address
+        {
+            public string Country { get; }
+
+            public DerivedAddress(string country, string street, string city)
+                : base(street, city)
+            {
+                Country = country;
+            }
+
+            protected override bool EqualsCore(Address other)
+            {
+                return other is DerivedAddress derived && base.EqualsCore(derived) && Country == derived.Country;
+            }
+        }
+    }
+}
--- CSharpFunctionalExtensions/ValueObject.cs
+++ CSharpFunctionalExtensions/ValueObject.cs
@@ -1,5 +1,11 @@
-﻿namespace CSharpFunctionalExtensions
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CSharpFunctionalExtensions
 {
+    [Obsolete("Use non-generic ValueObject instead: http://bit.ly/vo-new")]
     public abstract class ValueObject<T>
         where T : ValueObject<T>
     {
@@ -37,6 +43,52 @@
         }
 
         public static bool operator !=(ValueObject<T> a, ValueObject<T> b)
+        {
+            return !(a == b);
+        }
+    }
+
+    public abstract class ValueObject
+    {
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object obj)
+        {
+            var valueObject = obj as ValueObject;
+
+            if (ReferenceEquals(valueObject, null))
+                return false;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            return GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Aggregate(1, (current, obj) =>
+                {
+                    unchecked
+                    {
+                        return current * 23 + (obj?.GetHashCode() ?? 0);
+                    }
+                });
+        }
+
+        public static bool operator ==(ValueObject a, ValueObject b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValueObject a, ValueObject b)
         {
             return !(a == b);
         }
