8960115040a483db700ffbc0fb2c94cbec670705
Michael DiLeo <michael@pecsafety.com>
2019-04-22 10:01:06 -0500

    add support for maybe linq-like select chaining

--- CSharpFunctionalExtensions.Tests/MaybeTests/ExtensionsTests.cs
+++ CSharpFunctionalExtensions.Tests/MaybeTests/ExtensionsTests.cs
@@ -149,6 +149,29 @@ namespace CSharpFunctionalExtensions.Tests.MaybeTests
         }
 
         [Fact]
+        public void SelectMany_returns_no_value_if_maybe_has_no_value()
+        {
+	        Maybe<int> maybe1 = 1;
+	        var maybe2 = Maybe<int>.None;
+
+	        var maybe3 = from a in maybe1 from b in maybe2 select a + b;
+
+	        maybe3.HasValue.Should().BeFalse();
+        }
+
+        [Fact]
+        public void SelectMany_returns_projection_if_maybes_has_values()
+        {
+	        Maybe<int> maybe1 = 1;
+	        Maybe<int> maybe2 = 2;
+
+	        var maybe3 = from a in maybe1 from b in maybe2 select a + b;
+
+	        maybe3.HasValue.Should().BeTrue();
+	        maybe3.Value.Should().Be(3);
+        }
+
+		[Fact]
         public void Bind_returns_new_maybe()
         {
             Maybe<MyClass> maybe = new MyClass { Property = "Some value" };
