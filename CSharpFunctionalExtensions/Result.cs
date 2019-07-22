48942a49aa6602d9cf3d128b1ca10d5fb4210f93
altmann <mic.altmann@gmail.com>
2019-07-22 13:26:22 +0200

    Add CreateFailure()

--- CSharpFunctionalExtensions/Result.cs
+++ CSharpFunctionalExtensions/Result.cs
@@ -162,6 +162,28 @@ namespace CSharpFunctionalExtensions
             return Create(isSuccess, error);
         }
 
+        public static Result CreateFailure(bool isFailure, string error)
+        {
+            return isFailure
+                ? Fail(error)
+                : Ok();
+        }
+
+        public static Result CreateFailure(Func<bool> failurePredicate, string error)
+        {
+            return failurePredicate()
+                ? Fail(error)
+                : Ok();
+        }
+
+        public static async Task<Result> CreateFailure(Func<Task<bool>> failurePredicate, string error)
+        {
+            bool isFailure = await failurePredicate().ConfigureAwait(Result.DefaultConfigureAwait);
+            return isFailure
+                ? Fail(error)
+                : Ok();
+        }
+
         [DebuggerStepThrough]
         public static Result<T> Ok<T>(T value)
         {
@@ -190,6 +212,28 @@ namespace CSharpFunctionalExtensions
         {
             bool isSuccess = await predicate().ConfigureAwait(DefaultConfigureAwait);
             return Create(isSuccess, value, error);
+        }
+
+        public static Result<T> CreateFailure<T>(bool isFailure, T value, string error)
+        {
+            return isFailure
+                ? Fail<T>(error)
+                : Ok<T>(value);
+        }
+
+        public static Result<T> CreateFailure<T>(Func<bool> failurePredicate, T value, string error)
+        {
+            return failurePredicate()
+                ? Fail<T>(error)
+                : Ok<T>(value);
+        }
+
+        public static async Task<Result<T>> CreateFailure<T>(Func<Task<bool>> failurePredicate, T value, string error)
+        {
+            bool isFailure = await failurePredicate().ConfigureAwait(Result.DefaultConfigureAwait);
+            return isFailure
+                ? Fail<T>(error)
+                : Ok<T>(value);
         }
 
         [DebuggerStepThrough]
@@ -225,6 +269,29 @@ namespace CSharpFunctionalExtensions
             return isSuccess
                 ? Ok<T, E>(value)
                 : Fail<T, E>(error);
+        }
+
+        [DebuggerStepThrough]
+        public static Result<T, E> CreateFailure<T, E>(bool isFailure, T value, E error)
+        {
+            return isFailure
+                ? Fail<T, E>(error) 
+                : Ok<T, E>(value);
+        }
+
+        public static Result<T, E> CreateFailure<T, E>(Func<bool> failurePredicate, T value, E error)
+        {
+            return failurePredicate()
+                ? Fail<T, E>(error)
+                : Ok<T, E>(value);
+        }
+
+        public static async Task<Result<T, E>> CreateFailure<T, E>(Func<Task<bool>> failurePredicate, T value, E error)
+        {
+            bool isFailure = await failurePredicate().ConfigureAwait(Result.DefaultConfigureAwait);
+            return isFailure
+                ? Fail<T, E>(error)
+                : Ok<T, E>(value);
         }
 
         /// <summary>
