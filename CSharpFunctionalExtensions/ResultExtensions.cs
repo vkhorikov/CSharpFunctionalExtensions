cf562237fafd61873654768c0ae64436563549a2
Damian Plaza <Damian.Plaza@powel.com>
2019-07-25 06:39:35 +0200

    Add more overloads for Result Match extension methods

--- CSharpFunctionalExtensions/ResultExtensions.cs
+++ CSharpFunctionalExtensions/ResultExtensions.cs
@@ -396,7 +396,14 @@ namespace CSharpFunctionalExtensions
                 : Result.Fail<K>(result.Error);
         }
 
-        public static TE Match<TE, T>(this Result<T> result, Func<T, TE> Ok, Func<string, TE> Failure)
+        public static TOutput Match<TOutput, TError, TValue>(this Result<TValue, TError> result, Func<TValue, TOutput> Ok, Func<TError, TOutput> Failure)
+        {
+            return result.IsSuccess
+                ? Ok(result.Value)
+                : Failure(result.Error);
+        }
+
+        public static TOutput Match<TOutput, TValue>(this Result<TValue> result, Func<TValue, TOutput> Ok, Func<string, TOutput> Failure)
         {
             return result.IsSuccess
                 ? Ok(result.Value)
@@ -415,7 +422,19 @@ namespace CSharpFunctionalExtensions
             }
         }
 
-        public static TE Match<TE>(this Result result, Func<TE> Ok, Func<string, TE> Failure)
+        public static void Match<TError, TValue>(this Result<TValue, TError> result, Action<TValue> Ok, Action<TError> Failure)
+        {
+            if (result.IsSuccess)
+            {
+                Ok(result.Value);
+            }
+            else
+            {
+                Failure(result.Error);
+            }
+        }
+
+        public static T Match<T>(this Result result, Func<T> Ok, Func<string, T> Failure)
         {
             return result.IsSuccess
                 ? Ok()
