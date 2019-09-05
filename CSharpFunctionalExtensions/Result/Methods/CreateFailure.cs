dec928b2ecf9af478cbb945b1a46eb216841d75f
Matt Jenkins <7103159+space-alien@users.noreply.github.com>
2019-09-05 20:12:22 +0100

    CreateFailure() - overhaul

--- CSharpFunctionalExtensions/Result/Methods/CreateFailure.cs
+++ CSharpFunctionalExtensions/Result/Methods/CreateFailure.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace CSharpFunctionalExtensions
@@ -7,70 +6,39 @@ namespace CSharpFunctionalExtensions
     public partial struct Result
     {
         public static Result CreateFailure(bool isFailure, string error)
-        {
-            return isFailure
-                ? Fail(error)
-                : Ok();
-        }
+            => Create(!isFailure, error);
 
         public static Result CreateFailure(Func<bool> failurePredicate, string error)
-        {
-            return failurePredicate()
-                ? Fail(error)
-                : Ok();
-        }
+            => Create(!failurePredicate(), error);
 
         public static async Task<Result> CreateFailure(Func<Task<bool>> failurePredicate, string error)
         {
-            bool isFailure = await failurePredicate().ConfigureAwait(Result.DefaultConfigureAwait);
-            return isFailure
-                ? Fail(error)
-                : Ok();
+            bool isFailure = await failurePredicate().ConfigureAwait(DefaultConfigureAwait);
+            return Create(!isFailure, error);
         }
 
         public static Result<T> CreateFailure<T>(bool isFailure, T value, string error)
-        {
-            return isFailure
-                ? Fail<T>(error)
-                : Ok<T>(value);
-        }
+            => Create(!isFailure, value, error);
 
         public static Result<T> CreateFailure<T>(Func<bool> failurePredicate, T value, string error)
-        {
-            return failurePredicate()
-                ? Fail<T>(error)
-                : Ok<T>(value);
-        }
+            => Create(!failurePredicate(), value, error);
 
         public static async Task<Result<T>> CreateFailure<T>(Func<Task<bool>> failurePredicate, T value, string error)
         {
-            bool isFailure = await failurePredicate().ConfigureAwait(Result.DefaultConfigureAwait);
-            return isFailure
-                ? Fail<T>(error)
-                : Ok<T>(value);
+            bool isFailure = await failurePredicate().ConfigureAwait(DefaultConfigureAwait);
+            return Create(!isFailure, value, error);
         }
 
-        [DebuggerStepThrough]
         public static Result<T, E> CreateFailure<T, E>(bool isFailure, T value, E error)
-        {
-            return isFailure
-                ? Fail<T, E>(error)
-                : Ok<T, E>(value);
-        }
+            => Create(!isFailure, value, error);
 
         public static Result<T, E> CreateFailure<T, E>(Func<bool> failurePredicate, T value, E error)
-        {
-            return failurePredicate()
-                ? Fail<T, E>(error)
-                : Ok<T, E>(value);
-        }
+            => Create(!failurePredicate(), value, error);
 
         public static async Task<Result<T, E>> CreateFailure<T, E>(Func<Task<bool>> failurePredicate, T value, E error)
         {
-            bool isFailure = await failurePredicate().ConfigureAwait(Result.DefaultConfigureAwait);
-            return isFailure
-                ? Fail<T, E>(error)
-                : Ok<T, E>(value);
+            bool isFailure = await failurePredicate().ConfigureAwait(DefaultConfigureAwait);
+            return Create(!isFailure, value, error);
         }
     }
 }
