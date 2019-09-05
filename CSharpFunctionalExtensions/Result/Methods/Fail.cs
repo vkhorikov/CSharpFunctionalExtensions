4542abe7c2669feaea90c31212f93893c6dd4a57
Matt Jenkins <7103159+space-alien@users.noreply.github.com>
2019-09-05 20:06:21 +0100

    Fail() - minor consistency improvements

--- CSharpFunctionalExtensions/Result/Methods/Fail.cs
+++ CSharpFunctionalExtensions/Result/Methods/Fail.cs
@@ -13,13 +13,13 @@ namespace CSharpFunctionalExtensions
         [DebuggerStepThrough]
         public static Result<T> Fail<T>(string error)
         {
-            return new Result<T>(true, default(T), error);
+            return new Result<T>(true, default, error);
         }
 
         [DebuggerStepThrough]
         public static Result<T, E> Fail<T, E>(E error)
         {
-            return new Result<T, E>(true, default(T), error);
+            return new Result<T, E>(true, default, error);
         }
     }
 }
