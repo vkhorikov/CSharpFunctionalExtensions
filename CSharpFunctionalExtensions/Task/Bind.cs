9941714728bf1fb74997267cb1bc515a91d6abd5
Arkadiusz Świerczek <arkadiusz.swierczek@gmail.com>
2019-10-12 22:01:18 +0200

    Add proposition for dealing with tasks

---
+++ CSharpFunctionalExtensions/Task/Bind.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharpFunctionalExtensions
+{
+    public static partial class TaskExtensions
+    {
+        /// <summary>
+        ///     Selects task from a given function.
+        /// </summary>
+        public static async Task Bind(this Task task, Func<Task> func)
+        {
+            await task.ConfigureAwait(Result.DefaultConfigureAwait);
+            await func().ConfigureAwait(Result.DefaultConfigureAwait);
+        }
+
+        /// <summary>
+        ///     Selects task from a given function.
+        /// </summary>
+        public static async Task Bind<T>(this Task<T> task, Func<T, Task> func)
+        {
+            var value = await task.ConfigureAwait(Result.DefaultConfigureAwait);
+            await func(value).ConfigureAwait(Result.DefaultConfigureAwait);
+        }
+
+        /// <summary>
+        ///     Selects task from a given function.
+        /// </summary>
+        public static async Task<TR> Bind<TR>(this Task task, Func<Task<TR>> func)
+        {
+            await task.ConfigureAwait(Result.DefaultConfigureAwait);
+            return await func().ConfigureAwait(Result.DefaultConfigureAwait);
+        }
+
+        /// <summary>
+        ///     Selects task from a given function.
+        /// </summary>
+        public static async Task<TR> Bind<T, TR>(this Task<T> task, Func<T, Task<TR>> func)
+        {
+            var value = await task.ConfigureAwait(Result.DefaultConfigureAwait);
+            return await func(value).ConfigureAwait(Result.DefaultConfigureAwait);
+        }
+    }
+}
---
+++ CSharpFunctionalExtensions/Task/Map.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharpFunctionalExtensions
+{
+    public static partial class TaskExtensions
+    {
+        /// <summary>
+        ///     Creates a new task from the return value of a given function.
+        /// </summary>
+        public static async Task<TR> Map<TR>(this Task task, Func<TR> func)
+        {
+            await task.ConfigureAwait(Result.DefaultConfigureAwait);
+            return func();
+        }
+
+        /// <summary>
+        ///     Creates a new task from the return value of a given function.
+        /// </summary>
+        public static async Task<TR> Map<T, TR>(this Task<T> task, Func<T, TR> func)
+        {
+            var value = await task.ConfigureAwait(Result.DefaultConfigureAwait);
+            return func(value);
+        }
+    }
+}
