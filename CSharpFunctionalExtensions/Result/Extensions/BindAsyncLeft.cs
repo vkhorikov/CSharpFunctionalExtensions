c96c1f5bd629e105f1639275faf028eb3a5266ea
Arkadiusz Świerczek <arkadiusz.swierczek@gmail.com>
2019-10-12 21:39:02 +0200

    Add summary tag to Bind method

--- CSharpFunctionalExtensions/Result/Extensions/BindAsyncLeft.cs
+++ CSharpFunctionalExtensions/Result/Extensions/BindAsyncLeft.cs
@@ -5,30 +5,45 @@ namespace CSharpFunctionalExtensions
 {
     public static partial class AsyncResultExtensionsLeftOperand
     {
+        /// <summary>
+        ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+        /// </summary>
         public static async Task<Result<K, E>> Bind<T, K, E>(this Task<Result<T, E>> resultTask, Func<T, Result<K, E>> func)
         {
             Result<T, E> result = await resultTask.ConfigureAwait(Result.DefaultConfigureAwait);
             return result.Bind(func);
         }
 
+        /// <summary>
+        ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+        /// </summary>
         public static async Task<Result<K>> Bind<T, K>(this Task<Result<T>> resultTask, Func<T, Result<K>> func)
         {
             Result<T> result = await resultTask.ConfigureAwait(Result.DefaultConfigureAwait);
             return result.Bind(func);
         }
 
+        /// <summary>
+        ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+        /// </summary>
         public static async Task<Result<K>> Bind<K>(this Task<Result> resultTask, Func<Result<K>> func)
         {
             Result result = await resultTask.ConfigureAwait(Result.DefaultConfigureAwait);
             return result.Bind(func);
         }
 
+        /// <summary>
+        ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+        /// </summary>
         public static async Task<Result> Bind<T>(this Task<Result<T>> resultTask, Func<T, Result> func)
         {
             Result<T> result = await resultTask.ConfigureAwait(Result.DefaultConfigureAwait);
             return result.Bind(func);
         }
 
+        /// <summary>
+        ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+        /// </summary>
         public static async Task<Result> Bind(this Task<Result> resultTask, Func<Result> func)
         {
             Result result = await resultTask.ConfigureAwait(Result.DefaultConfigureAwait);
