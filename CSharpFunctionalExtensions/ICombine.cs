b8e499de6a5cc16f8ccec31e45509a8f4bd026c6
mukmyash <mukminovartur@gmail.com>
2019-10-31 22:53:18 +0500

    Add Combine methode with custom Error class

---
+++ CSharpFunctionalExtensions/ICombine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFunctionalExtensions
+{
+    public interface ICombine
+    {
+        ICombine Combine(ICombine value);
+    }
+}
--- CSharpFunctionalExtensions/Result/Extensions/CombineAsyncLeft.cs
+++ CSharpFunctionalExtensions/Result/Extensions/CombineAsyncLeft.cs
@@ -18,6 +18,19 @@ namespace CSharpFunctionalExtensions
             return results.Combine(errorMessageSeparator);
         }
 
+        public static async Task<Result<IEnumerable<T>, E>> Combine<T, E>(this IEnumerable<Task<Result<T, E>>> tasks, Func<IEnumerable<E>, E> composerError)
+        {
+            Result<T, E>[] results = await Task.WhenAll(tasks).ConfigureAwait(Result.DefaultConfigureAwait);
+            return results.Combine(composerError);
+        }
+
+        public static async Task<Result<IEnumerable<T>, E>> Combine<T, E>(this IEnumerable<Task<Result<T, E>>> tasks)
+            where E : ICombine
+        {
+            Result<T, E>[] results = await Task.WhenAll(tasks).ConfigureAwait(Result.DefaultConfigureAwait);
+            return results.Combine();
+        }
+
         public static async Task<Result<IEnumerable<T>>> Combine<T>(this IEnumerable<Task<Result<T>>> tasks, string errorMessageSeparator = null)
         {
             Result<T>[] results = await Task.WhenAll(tasks).ConfigureAwait(Result.DefaultConfigureAwait);
@@ -28,6 +41,19 @@ namespace CSharpFunctionalExtensions
         {
             IEnumerable<Result> results = await task.ConfigureAwait(Result.DefaultConfigureAwait);
             return results.Combine(errorMessageSeparator);
+        }
+
+        public static async Task<Result<IEnumerable<T>, E>> Combine<T, E>(this Task<IEnumerable<Result<T, E>>> task, Func<IEnumerable<E>, E> composerError)
+        {
+            IEnumerable<Result<T, E>> results = await task.ConfigureAwait(Result.DefaultConfigureAwait);
+            return results.Combine(composerError);
+        }
+
+        public static async Task<Result<IEnumerable<T>, E>> Combine<T, E>(this Task<IEnumerable<Result<T, E>>> task)
+            where E : ICombine
+        {
+            IEnumerable<Result<T, E>> results = await task.ConfigureAwait(Result.DefaultConfigureAwait);
+            return results.Combine();
         }
 
         public static async Task<Result<IEnumerable<T>>> Combine<T>(this Task<IEnumerable<Result<T>>> task, string errorMessageSeparator = null)
@@ -42,16 +68,55 @@ namespace CSharpFunctionalExtensions
             return await tasks.Combine(errorMessageSeparator).ConfigureAwait(Result.DefaultConfigureAwait);
         }
 
+        public static async Task<Result<IEnumerable<T>, E>> Combine<T, E>(this Task<IEnumerable<Task<Result<T, E>>>> task, Func<IEnumerable<E>, E> composerError)
+        {
+            IEnumerable<Task<Result<T, E>>> tasks = await task.ConfigureAwait(Result.DefaultConfigureAwait);
+            return await tasks.Combine(composerError).ConfigureAwait(Result.DefaultConfigureAwait);
+        }
+
+        public static async Task<Result<IEnumerable<T>, E>> Combine<T, E>(this Task<IEnumerable<Task<Result<T, E>>>> task)
+            where E : ICombine
+        {
+            IEnumerable<Task<Result<T, E>>> tasks = await task.ConfigureAwait(Result.DefaultConfigureAwait);
+            return await tasks.Combine().ConfigureAwait(Result.DefaultConfigureAwait);
+        }
+
         public static async Task<Result<IEnumerable<T>>> Combine<T>(this Task<IEnumerable<Task<Result<T>>>> task, string errorMessageSeparator = null)
         {
             IEnumerable<Task<Result<T>>> tasks = await task.ConfigureAwait(Result.DefaultConfigureAwait);
             return await tasks.Combine(errorMessageSeparator).ConfigureAwait(Result.DefaultConfigureAwait);
         }
 
+        public static async Task<Result<K, E>> Combine<T, K, E>(this IEnumerable<Task<Result<T, E>>> tasks, Func<IEnumerable<T>, K> composer, Func<IEnumerable<E>, E> composerError)
+        {
+            IEnumerable<Result<T, E>> results = await Task.WhenAll(tasks).ConfigureAwait(Result.DefaultConfigureAwait);
+            return results.Combine(composer, composerError);
+        }
+
+        public static async Task<Result<K, E>> Combine<T, K, E>(this IEnumerable<Task<Result<T, E>>> tasks, Func<IEnumerable<T>, K> composer)
+            where E : ICombine
+        {
+            IEnumerable<Result<T, E>> results = await Task.WhenAll(tasks).ConfigureAwait(Result.DefaultConfigureAwait);
+            return results.Combine(composer);
+        }
+
         public static async Task<Result<K>> Combine<T, K>(this IEnumerable<Task<Result<T>>> tasks, Func<IEnumerable<T>, K> composer, string errorMessageSeparator = null)
         {
             IEnumerable<Result<T>> results = await Task.WhenAll(tasks).ConfigureAwait(Result.DefaultConfigureAwait);
             return results.Combine(composer, errorMessageSeparator);
+        }
+
+        public static async Task<Result<K, E>> Combine<T, K, E>(this Task<IEnumerable<Task<Result<T, E>>>> task, Func<IEnumerable<T>, K> composer, Func<IEnumerable<E>, E> composerError)
+        {
+            IEnumerable<Task<Result<T, E>>> tasks = await task.ConfigureAwait(Result.DefaultConfigureAwait);
+            return await tasks.Combine(composer, composerError).ConfigureAwait(Result.DefaultConfigureAwait);
+        }
+
+        public static async Task<Result<K, E>> Combine<T, K, E>(this Task<IEnumerable<Task<Result<T, E>>>> task, Func<IEnumerable<T>, K> composer)
+            where E : ICombine
+        {
+            IEnumerable<Task<Result<T, E>>> tasks = await task.ConfigureAwait(Result.DefaultConfigureAwait);
+            return await tasks.Combine(composer).ConfigureAwait(Result.DefaultConfigureAwait);
         }
 
         public static async Task<Result<K>> Combine<T, K>(this Task<IEnumerable<Task<Result<T>>>> task, Func<IEnumerable<T>, K> composer, string errorMessageSeparator = null)
