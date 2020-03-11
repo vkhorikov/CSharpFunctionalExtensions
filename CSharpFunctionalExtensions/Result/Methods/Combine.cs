f937ad2a0d85fac356e182a89f620f3ef5629c49
Matt Jenkins <7103159+space-alien@users.noreply.github.com>
2020-03-11 10:36:46 +0000

    Add documentation to Result methods

--- CSharpFunctionalExtensions/Result/Methods/Combine.cs
+++ CSharpFunctionalExtensions/Result/Methods/Combine.cs
@@ -6,12 +6,29 @@ namespace CSharpFunctionalExtensions
 {
     public partial struct Result
     {
+        /// <summary>
+        ///     Combines several results (and any errors) into a single result.
+        ///     The returned result will be a failure if any of the input <paramref name="results"/> are failures.
+        ///     The E error class must implement ICombine to provide an accumulator function for combining any errors.</summary>
+        /// <param name="results">
+        ///     The Results to be combined.</param>
+        /// <returns>
+        ///     A Result that is a success when all the input <paramref name="results"/> are also successes.</returns>
         public static Result<bool, E> Combine<T, E>(IEnumerable<Result<T, E>> results)
             where E : ICombine
         {
             return Combine(results, (errors) => errors.Aggregate((x, y) => (E)x.Combine(y)));
         }
 
+        /// <summary>
+        ///     Combines several results (and any errors) into a single result.
+        ///     The returned result will be a failure if any of the input <paramref name="results"/> are failures.</summary>
+        /// <param name="results">
+        ///     The Results to be combined.</param>
+        /// <param name="composerError">
+        ///     A function that combines any errors.</param>
+        /// <returns>
+        ///     A Result that is a success when all the input <paramref name="results"/> are also successes.</returns>
         public static Result<bool, E> Combine<T, E>(IEnumerable<Result<T, E>> results, Func<IEnumerable<E>, E> composerError)
         {
             List<Result<T, E>> failedResults = results.Where(x => x.IsFailure).ToList();
@@ -23,6 +40,15 @@ namespace CSharpFunctionalExtensions
             return Failure<bool, E>(errorMessage);
         }
 
+        /// <summary>
+        ///     Combines several results (and any error messages) into a single result.
+        ///     The returned result will be a failure if any of the input <paramref name="results"/> are failures.</summary>
+        /// <param name="results">
+        ///     The Results to be combined.</param>
+        /// <param name="errorMessagesSeparator">
+        ///     A string that is used to separate any concatenated error messages. If omitted, the default <see cref="Result.ErrorMessagesSeparator" /> is used.</param>
+        /// <returns>
+        ///     A Result that is a success when all the input <paramref name="results"/> are also successes.</returns>
         public static Result Combine(IEnumerable<Result> results, string errorMessagesSeparator = null)
         {
             List<Result> failedResults = results.Where(x => x.IsFailure).ToList();
@@ -34,6 +60,15 @@ namespace CSharpFunctionalExtensions
             return Failure(errorMessage);
         }
 
+        /// <summary>
+        ///     Combines several results (and any error messages) into a single result.
+        ///     The returned result will be a failure if any of the input <paramref name="results"/> are failures.</summary>
+        /// <param name="results">
+        ///     The Results to be combined.</param>
+        /// <param name="errorMessagesSeparator">
+        ///     A string that is used to separate any concatenated error messages. If omitted, the default <see cref="Result.ErrorMessagesSeparator" /> is used.</param>
+        /// <returns>
+        ///     A Result that is a success when all the input <paramref name="results"/> are also successes.</returns>
         public static Result Combine<T>(IEnumerable<Result<T>> results, string errorMessagesSeparator = null)
         {
             var untyped = results.Select(result => (Result)result);
@@ -41,36 +76,72 @@ namespace CSharpFunctionalExtensions
         }
 
         /// <summary>
-        /// Combines several results (and any error messages) into a single result.
-        /// The returned result will be a failure if any of the input <paramref name="results"/> are failures.
-        /// Error messages are concatenated with the default <see cref="Result.ErrorMessagesSeparator" /> between each message.
-        /// </summary>
-        /// <param name="results">The Results to be combined.</param>
-        /// <returns>A Result that is a success when all the input <paramref name="results"/> are also successes.</returns>
+        ///     Combines several results (and any error messages) into a single result.
+        ///     The returned result will be a failure if any of the input <paramref name="results"/> are failures.
+        ///     Error messages are concatenated with the default <see cref="Result.ErrorMessagesSeparator" /> between each message.</summary>
+        /// <param name="results">
+        ///     The Results to be combined.</param>
+        /// <returns>
+        ///     A Result that is a success when all the input <paramref name="results"/> are also successes.</returns>
         public static Result Combine(params Result[] results)
             => Combine(results, ErrorMessagesSeparator);
 
+        /// <summary>
+        ///     Combines several results (and any errors) into a single result.
+        ///     The returned result will be a failure if any of the input <paramref name="results"/> are failures.
+        ///     The E error class must implement ICombine to provide an accumulator function for combining any errors.</summary>
+        /// <param name="results">
+        ///     The Results to be combined.</param>
+        /// <returns>
+        ///     A Result that is a success when all the input <paramref name="results"/> are also successes.</returns>
         public static Result<bool, E> Combine<T, E>(params Result<T, E>[] results)
             where E : ICombine
             => Combine<T, E>(results, (errors) => errors.Aggregate((x, y) => (E)x.Combine(y)));
 
+        /// <summary>
+        ///     Combines several results (and any errors) into a single result.
+        ///     The returned result will be a failure if any of the input <paramref name="results"/> are failures.</summary>
+        /// <param name="composerError">
+        ///     A function that combines any errors.</param>
+        /// <param name="results">
+        ///     The Results to be combined.</param>
+        /// <returns>
+        ///     A Result that is a success when all the input <paramref name="results"/> are also successes.</returns>
         public static Result<bool, E> Combine<T, E>(Func<IEnumerable<E>, E> composerError, params Result<T, E>[] results)
             => Combine<T, E>(results, composerError);
 
         /// <summary>
-        /// Combines several results (and any error messages) into a single result.
-        /// The returned result will be a failure if any of the input <paramref name="results"/> are failures.
-        /// Error messages are concatenated with the specified <paramref name="errorMessagesSeparator"/> between each message.
-        /// </summary>
-        /// <param name="errorMessagesSeparator">The string to use as a separator. If omitted, the default <see cref="Result.ErrorMessagesSeparator" /> is used instead.</param>
-        /// <param name="results">The Results to be combined.</param>
-        /// <returns>A Result that is a success when all the input <paramref name="results"/> are also successes.</returns>
+        ///     Combines several results (and any error messages) into a single result.
+        ///     The returned result will be a failure if any of the input <paramref name="results"/> are failures.</summary>
+        /// <param name="errorMessagesSeparator">
+        ///     A string that is used to separate any concatenated error messages. If omitted, the default <see cref="Result.ErrorMessagesSeparator" /> is used.</param>
+        /// <param name="results">
+        ///     The Results to be combined.</param>
+        /// <returns>
+        ///     A Result that is a success when all the input <paramref name="results"/> are also successes.</returns>
         public static Result Combine(string errorMessagesSeparator, params Result[] results)
             => Combine(results, errorMessagesSeparator);
 
+        /// <summary>
+        ///     Combines several results (and any error messages) into a single result.
+        ///     The returned result will be a failure if any of the input <paramref name="results"/> are failures.
+        ///     Error messages are concatenated with the default <see cref="Result.ErrorMessagesSeparator" /> between each message.</summary>
+        /// <param name="results">
+        ///     The Results to be combined.</param>
+        /// <returns>
+        ///     A Result that is a success when all the input <paramref name="results"/> are also successes.</returns>
         public static Result Combine<T>(params Result<T>[] results)
             => Combine(results, ErrorMessagesSeparator);
 
+        /// <summary>
+        ///     Combines several results (and any error messages) into a single result.
+        ///     The returned result will be a failure if any of the input <paramref name="results"/> are failures.</summary>
+        /// <param name="errorMessagesSeparator">
+        ///     A string that is used to separate any concatenated error messages. If omitted, the default <see cref="Result.ErrorMessagesSeparator" /> is used.</param>
+        /// <param name="results">
+        ///     The Results to be combined.</param>
+        /// <returns>
+        ///     A Result that is a success when all the input <paramref name="results"/> are also successes.</returns>
         public static Result Combine<T>(string errorMessagesSeparator, params Result<T>[] results)
             => Combine(results, errorMessagesSeparator);
     }
--- CSharpFunctionalExtensions/Result/Methods/Success.cs
+++ CSharpFunctionalExtensions/Result/Methods/Success.cs
@@ -2,16 +2,25 @@
 {
     public partial struct Result
     {
+        /// <summary>
+        ///     Creates a success result.
+        /// </summary>
         public static Result Success()
         {
             return new Result(false, default);
         }
 
+        /// <summary>
+        ///     Creates a success result containing the given value.
+        /// </summary>
         public static Result<T> Success<T>(T value)
         {
             return new Result<T>(false, default, value);
         }
 
+        /// <summary>
+        ///     Creates a success result containing the given value.
+        /// </summary>
         public static Result<T, E> Success<T, E>(T value)
         {
             return new Result<T, E>(false, default, value);
