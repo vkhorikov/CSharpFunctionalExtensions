1d698c01fdaa88a5e4812700ad8f628c13e6c488
Vladimir <vladimir.khorikov@gmail.com>
2020-06-27 19:15:25 -0400

    Fixes #204 Wrong return Type in 2.8.0

---
+++ CSharpFunctionalExtensions.Tests/ResultTests/ImplicitConversionTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpFunctionalExtensions.Tests.ResultTests
+{
+    public class ImplicitConversionTests
+    {
+        [Fact]
+        public void Implicit_conversion_of_dynamic_result()
+        {
+            Result<dynamic> result = Result.Success<dynamic>((dynamic)"result");
+
+            Type type = result.Value.GetType();
+            type.Should().Be(typeof(string));
+        }
+    }
+}
