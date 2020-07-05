c91309fbcbb0b5d52bfba8054ef98ae563060469
Sviataslau Hankovich <Sviataslau.Hankovich@rtl.nl>
2020-07-05 19:18:53 +0300

    #209 Add implicit conversions from E to Result<T, E>

--- CSharpFunctionalExtensions.Tests/ResultTests/ImplicitConversionTests.cs
+++ CSharpFunctionalExtensions.Tests/ResultTests/ImplicitConversionTests.cs
@@ -14,5 +14,38 @@ namespace CSharpFunctionalExtensions.Tests.ResultTests
             Type type = result.Value.GetType();
             type.Should().Be(typeof(string));
         }
+        
+        [Fact]
+        public void Implicit_conversion_T_is_converted_to_Success_result_of_T()
+        {
+            var value = "result";
+
+            Result<string> result = value;
+
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().Be(value);
+        }
+        
+        [Fact]
+        public void Implicit_conversion_T_is_converted_to_Success_result_of_T_E()
+        {
+            var value = "result";
+
+            Result<string, int> result = value;
+
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().Be(value);
+        }
+        
+        [Fact]
+        public void Implicit_conversion_E_is_converted_to_Failure_result_of_T_E()
+        {
+            int value = 42;
+
+            Result<string, int> result = value;
+
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(value);
+        }
     }
 }
