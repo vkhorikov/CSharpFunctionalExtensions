16e0c502a48d3312a54379abaf41c5120aa5c003
Steven Giesel <stgiesel35@gmail.com>
2020-07-31 10:44:34 +0200

    Added Aggregate function for Result array

---
+++ CSharpFunctionalExtensions/Result/Extensions/Aggregate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpFunctionalExtensions
+{
+    public static partial class ResultExtensions
+    {
+        /// <summary>
+        ///     Creates a new result from a enumeration of results. If one fails the new result will have the combined result
+        /// </summary>
+        public static Result<IEnumerable<TValue>> Aggregate<TValue>(this IEnumerable<Result<TValue>> results, string errorMessagesSeparator = null)
+        {
+            Result<TValue>[] resultsArray = results.ToArray();
+            Result combined = Result.Combine(resultsArray, errorMessagesSeparator);
+            if (combined.IsFailure)
+            {
+                return Result.Failure<IEnumerable<TValue>>(combined.Error);
+            }
+
+            return Result.Success<IEnumerable<TValue>>(resultsArray.Select(result => result.Value).ToList());
+        }
+
+        /// <summary>
+        ///     Creates a new result from a enumeration of results. If one fails the new result will have the combined result
+        /// </summary>
+        public static Result Aggregate(this IEnumerable<Result> results, string errorMessagesSeparator = null)
+        {
+            Result[] resultsArray = results.ToArray();
+            Result combined = Result.Combine(resultsArray, errorMessagesSeparator);
+            if (combined.IsFailure)
+            {
+                return Result.Failure(combined.Error);
+            }
+
+            return Result.Success();
+        }
+    }
+}
