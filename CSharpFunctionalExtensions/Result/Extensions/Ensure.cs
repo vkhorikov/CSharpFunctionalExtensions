1eddae4fb99f47beae72f31fec27d6481719c7ce
Darick Carpenter <carpenterd@byui.edu>
2020-09-28 12:25:28 -0600

    Added Ensure method where the object is available to generate error message.

--- CSharpFunctionalExtensions/Result/Extensions/Ensure.cs
+++ CSharpFunctionalExtensions/Result/Extensions/Ensure.cs
@@ -35,6 +35,20 @@ namespace CSharpFunctionalExtensions
         /// <summary>
         ///     Returns a new failure result if the predicate is false. Otherwise returns the starting result.
         /// </summary>
+        public static Result<T> Ensure<T>(this Result<T> result, Func<T, bool> predicate, Func<T, string> errorMessage)
+        {
+            if (result.IsFailure)
+                return result;
+
+            if (!predicate(result.Value))
+                return Result.Failure<T>(errorMessage(result.Value));
+
+            return result;
+        }
+
+        /// <summary>
+        ///     Returns a new failure result if the predicate is false. Otherwise returns the starting result.
+        /// </summary>
         public static Result Ensure(this Result result, Func<bool> predicate, string errorMessage)
         {
             if (result.IsFailure)
