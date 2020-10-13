711b1cf3fc6d8b51a39f56f0a72620ba69b5bc02
Vladimir <vladimir.khorikov@gmail.com>
2020-10-13 14:06:04 -0400

    #203 ValueObject should implement IComparable

--- CSharpFunctionalExtensions/ValueObject/ValueObject.cs
+++ CSharpFunctionalExtensions/ValueObject/ValueObject.cs
@@ -18,7 +18,7 @@ namespace CSharpFunctionalExtensions
         {
             var valueObject = obj as T;
 
-            if (ReferenceEquals(valueObject, null))
+            if (valueObject is null)
                 return false;
 
             if (ValueObject.GetUnproxiedType(this) != ValueObject.GetUnproxiedType(obj))
@@ -43,10 +43,10 @@ namespace CSharpFunctionalExtensions
 
         public static bool operator ==(ValueObject<T> a, ValueObject<T> b)
         {
-            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+            if (a is null && b is null)
                 return true;
 
-            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+            if (a is null || b is null)
                 return false;
 
             return a.Equals(b);
@@ -59,7 +59,7 @@ namespace CSharpFunctionalExtensions
     }
 
     [Serializable]
-    public abstract class ValueObject
+    public abstract class ValueObject : IComparable, IComparable<ValueObject>
     {
         private int? _cachedHashCode;
 
@@ -87,7 +87,7 @@ namespace CSharpFunctionalExtensions
                     {
                         unchecked
                         {
-                            return (current * 23) + (obj?.GetHashCode() ?? 0);
+                            return current * 23 + (obj?.GetHashCode() ?? 0);
                         }
                     });
             }
@@ -95,12 +95,60 @@ namespace CSharpFunctionalExtensions
             return _cachedHashCode.Value;
         }
 
+        public int CompareTo(object obj)
+        {
+            Type thisType = GetUnproxiedType(this);
+            Type otherType = GetUnproxiedType(obj);
+
+            if (thisType != otherType)
+                return string.Compare(thisType.ToString(), otherType.ToString(), StringComparison.Ordinal);
+
+            var other = (ValueObject)obj;
+
+            object[] components = GetEqualityComponents().ToArray();
+            object[] otherComponents = other.GetEqualityComponents().ToArray();
+
+            for (int i = 0; i < components.Length; i++)
+            {
+                int comparison = CompareComponents(components[i], otherComponents[i]);
+                if (comparison != 0)
+                    return comparison;
+            }
+
+            return 0;
+        }
+
+        private int CompareComponents(object object1, object object2)
+        {
+            if (object1 is null && object2 is null)
+                return 0;
+
+            if (object1 is null)
+                return -1;
+
+            if (object2 is null)
+                return 1;
+
+            var component1 = object1 as IComparable;
+            var component2 = object2 as IComparable;
+
+            if (component1 == null || component2 == null)
+                throw new InvalidOperationException($"Not all components in {GetUnproxiedType(this)} implement IComparable");
+
+            return component1.CompareTo(component2);
+        }
+
+        public int CompareTo(ValueObject other)
+        {
+            return CompareTo(other as object);
+        }
+
         public static bool operator ==(ValueObject a, ValueObject b)
         {
-            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+            if (a is null && b is null)
                 return true;
 
-            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+            if (a is null || b is null)
                 return false;
 
             return a.Equals(b);
