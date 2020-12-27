47402224c9146e438495aba048aa27d605072e66
Vladimir <vladimir.khorikov@gmail.com>
2020-12-27 12:56:47 -0500

    Added support for .NET Framework 4.0 and .net 5.0 Upgraded test packages Removed link tests

--- CSharpFunctionalExtensions/Result/Extensions/BindWithTransactionScope.cs
+++ CSharpFunctionalExtensions/Result/Extensions/BindWithTransactionScope.cs
@@ -1,4 +1,4 @@
-#if NETSTANDARD2_0
+#if NETSTANDARD2_0 || NET50
 using System;
 
 namespace CSharpFunctionalExtensions
--- CSharpFunctionalExtensions/Result/Extensions/TapIfAsyncRight.cs
+++ CSharpFunctionalExtensions/Result/Extensions/TapIfAsyncRight.cs
@@ -1,3 +1,4 @@
+#if !NET40
 using System;
 using System.Threading.Tasks;
 
@@ -105,3 +106,4 @@ namespace CSharpFunctionalExtensions
         }
     }
 }
+#endif
