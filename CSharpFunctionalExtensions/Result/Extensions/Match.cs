370abb135f945eb764b88c4ffce6973ff82f574e
Stéphane Mitermite <stephane.mitermite@berger-levrault.com>
2021-02-13 18:59:56 +0100

    Adds comments to Match methods

--- CSharpFunctionalExtensions/Result/Extensions/Match.cs
+++ CSharpFunctionalExtensions/Result/Extensions/Match.cs
@@ -4,6 +4,9 @@ namespace CSharpFunctionalExtensions
 {
     public static partial class ResultExtensions
     {
+        /// <summary>
+        ///     Returns the result of the given <paramref name="onSuccess"/> function if the calling Result is a success. Otherwise, it returns the result of the given <paramref name="onFailure"/> function.
+        /// </summary>
         public static K Match<T, K, E>(this Result<T, E> result, Func<T, K> onSuccess, Func<E, K> onFailure)
         {
             return result.IsSuccess
@@ -11,6 +14,9 @@ namespace CSharpFunctionalExtensions
                 : onFailure(result.Error);
         }
 
+        /// <summary>
+        ///     Returns the result of the given <paramref name="onSuccess"/> function if the calling Result is a success. Otherwise, it returns the result of the given <paramref name="onFailure"/> function.
+        /// </summary>
         public static K Match<K, T>(this Result<T> result, Func<T, K> onSuccess, Func<string, K> onFailure)
         {
             return result.IsSuccess
@@ -18,6 +24,9 @@ namespace CSharpFunctionalExtensions
                 : onFailure(result.Error);
         }
 
+        /// <summary>
+        ///     Returns the result of the given <paramref name="onSuccess"/> function if the calling Result is a success. Otherwise, it returns the result of the given <paramref name="onFailure"/> function.
+        /// </summary>
         public static T Match<T>(this Result result, Func<T> onSuccess, Func<string, T> onFailure)
         {
             return result.IsSuccess
@@ -25,6 +34,9 @@ namespace CSharpFunctionalExtensions
                 : onFailure(result.Error);
         }
 
+        /// <summary>
+        ///     Invokes the given <paramref name="onSuccess"/> action if the calling Result is a success. Otherwise, it invokes the given <paramref name="onFailure"/> action.
+        /// </summary>
         public static void Match<T, E>(this Result<T, E> result, Action<T> onSuccess, Action<E> onFailure)
         {
             if (result.IsSuccess)
@@ -33,6 +45,9 @@ namespace CSharpFunctionalExtensions
                 onFailure(result.Error);
         }
 
+        /// <summary>
+        ///     Invokes the given <paramref name="onSuccess"/> action if the calling Result is a success. Otherwise, it invokes the given <paramref name="onFailure"/> action.
+        /// </summary>
         public static void Match<T>(this Result<T> result, Action<T> onSuccess, Action<string> onFailure)
         {
             if (result.IsSuccess)
@@ -41,6 +56,9 @@ namespace CSharpFunctionalExtensions
                 onFailure(result.Error);
         }
 
+        /// <summary>
+        ///     Invokes the given <paramref name="onSuccess"/> action if the calling Result is a success. Otherwise, it invokes the given <paramref name="onFailure"/> action.
+        /// </summary>
         public static void Match(this Result result, Action onSuccess, Action<string> onFailure)
         {
             if (result.IsSuccess)
