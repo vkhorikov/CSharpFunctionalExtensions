d60bfbba9daa67d4c0bb05c721dc8a9851b96aab
Fehim <fehim_dervisbegovic@msn.com>
2021-04-23 00:55:04 +0200

    Success and Failure async methods

---
+++ CSharpFunctionalExtensions/Result/Methods/SuccessAsync.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+namespace CSharpFunctionalExtensions
+{
+    public partial struct Result
+    {
+        /// <summary>
+        ///     Creates a success async result.
+        /// </summary>
+        public static Task<Result> SuccessAsync()
+        {
+            return new Task<Result>(() => Success());
+        }
+
+        /// <summary>
+        ///     Creates a success async result containing the given value.
+        /// </summary>
+        public static Task<Result<T>> SuccessAsync<T>(T value)
+        {
+            return new Task<Result<T>>(() => Success<T>(value));
+        }
+        
+        /// <summary>
+        ///     Creates a success async result containing the given value.
+        /// </summary>
+        public static Task<Result<T, E>> SuccessAsync<T, E>(T value)
+        {
+            return new Task<Result<T,E>>(() => Success<T, E>(value));
+        }
+    }
+}
