d443c4d9e4e77537d2c3f84dcc13a74afdae58b4
José Manuel Nieto <superjmn@outlook.com>
2021-10-11 11:01:53 +0200

    With (Combine)

---
+++ CSharpFunctionalExtensions.Tests/ResultTests/Extensions/With/ResultOfTTest.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CSharpFunctionalExtensions.Tests.ResultTests.Extensions.With
+{
+    public class ResultOfTTest
+    {
+        [Fact]
+        public void Results_of_same_type_are_combined_correctly()
+        {
+            var r1 = Result.Success("Hello");
+            var r2 = Result.Success("world");
+
+            var actual = r1.With(r2);
+            actual.IsSuccess.Should().BeTrue();
+            actual.Value.Should().Be(("Hello", "world"));
+        }
+
+        [Fact]
+        public void Results_of_different_type_are_mapped_correctly()
+        {
+            var r1 = Result.Success("Hello");
+            var r2 = Result.Success("world");
+
+            var actual = r1.WithMap(r2, (a, b) => a + b);
+            actual.IsSuccess.Should().BeTrue();
+            actual.Value.Should().Be("Helloworld");
+        }
+
+        [Fact]
+        public void Results_of_different_type_are_bound_correctly()
+        {
+            var r1 = Result.Success("Hello");
+            var r2 = Result.Success("world");
+
+            var actual = r1.WithBind(r2, (a, b) => Result.Success(a + b));
+            actual.IsSuccess.Should().BeTrue();
+            actual.Value.Should().Be("Helloworld");
+        }
+    }
+}
---
+++ CSharpFunctionalExtensions/Result/Extensions/With/AsyncResultOfTE.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharpFunctionalExtensions
+{
+    public static partial class ResultExtensions
+    {
+        public static Task<Result<TResult, E>> WithMap<T1, T2, E, TResult>(this Result<T1, E> a,
+            Result<T2, E> b,
+            Func<T1, T2, Task<TResult>> map, Func<E, E, E> combineError)
+        {
+            var mapSuccess =
+                a.BindError(e1 => b
+                        .MapError(e2 => combineError(e1, e2))
+                        .Bind(_ => Result.Failure<T1, E>(e1)))
+                    .Bind(x => b
+                        .Map(y => map(x, y))
+                        .MapError(el => el));
+
+            return mapSuccess;
+        }
+
+        public static Task<Result<TResult, E>> WithBind<T1, T2, E, TResult>(this Result<T1, E> a,
+            Result<T2, E> b,
+            Func<T1, T2, Task<Result<TResult, E>>> map, Func<E, E, E> combineError)
+        {
+            var mapSuccess =
+                a.BindError(e1 => b
+                        .MapError(e2 => combineError(e1, e2))
+                        .Bind(_ => Result.Failure<T1, E>(e1)))
+                    .Bind(x => b
+                        .Bind(y => map(x, y))
+                        .MapError(el => el));
+
+            return mapSuccess;
+        }
+    }
+}
