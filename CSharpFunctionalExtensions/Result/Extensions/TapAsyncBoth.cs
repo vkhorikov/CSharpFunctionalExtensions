f4c42293fce66a3e155e937d0d7d664523060429
Sam Viesselman <sviesselman@kahlerautomation.com>
2021-10-12 21:47:35 -0500

    Add Tap Extension Methods for UnitResult

--- CSharpFunctionalExtensions/Result/Extensions/TapAsyncBoth.cs
+++ CSharpFunctionalExtensions/Result/Extensions/TapAsyncBoth.cs
@@ -47,6 +47,19 @@ namespace CSharpFunctionalExtensions
         /// <summary>
         ///     Executes the given action if the calling result is a success. Returns the calling result.
         /// </summary>
+        public static async Task<UnitResult<E>> Tap<E>(this Task<UnitResult<E>> resultTask, Func<Task> func)
+        {
+            UnitResult<E> result = await resultTask.DefaultAwait();
+
+            if (result.IsSuccess)
+                await func().DefaultAwait();
+
+            return result;
+        }
+
+        /// <summary>
+        ///     Executes the given action if the calling result is a success. Returns the calling result.
+        /// </summary>
         public static async Task<Result<T, E>> Tap<T, E>(this Task<Result<T, E>> resultTask, Func<Task> func)
         {
             Result<T, E> result = await resultTask.DefaultAwait();
