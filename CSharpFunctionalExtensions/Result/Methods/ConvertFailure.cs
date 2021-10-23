e443b55498c046c2ce0242380a218c4d7a5e3e4e
Sam Viesselman <sviesselman@kahlerautomation.com>
2021-10-23 16:54:44 -0500

    Add convert failure methods for unit result

--- CSharpFunctionalExtensions/Result/Methods/ConvertFailure.cs
+++ CSharpFunctionalExtensions/Result/Methods/ConvertFailure.cs
@@ -54,4 +54,18 @@ namespace CSharpFunctionalExtensions
             return Result.Failure<K, E>(Error);
         }
     }
+
+    public partial struct UnitResult<E> 
+    {
+        /// <summary>
+        ///     Throws if the result is a success. Else returns a new failure result of the given type.
+        /// </summary>
+        public Result<K, E> ConvertFailure<K>()
+        {
+            if (IsSuccess)
+                throw new InvalidOperationException(Result.Messages.ConvertFailureExceptionOnSuccess);
+
+            return Result.Failure<K, E>(Error);
+        }
+    }
 }
