ce0178e39b20278e4d11f8ba5c56e5d24450b58c
hankovich <sviataslau.hankovich@fabros-team.com>
2021-11-16 20:33:25 +0300

    Add compensate extensions and tests for them.

---
+++ CSharpFunctionalExtensions/Result/Extensions/Compensate.cs
@@ -0,0 +1,107 @@
+using System;
+
+namespace CSharpFunctionalExtensions
+{
+    public static partial class ResultExtensions
+    {
+        public static Result Compensate(this Result result, Func<string, Result> func)
+        {
+            if (result.IsSuccess)
+            {
+                return Result.Success();
+            }
+
+            return func(result.Error);
+        }
+
+        public static UnitResult<E> Compensate<E>(this Result result, Func<string, UnitResult<E>> func)
+        {
+            if (result.IsSuccess)
+            {
+                return UnitResult.Success<E>();
+            }
+
+            return func(result.Error);
+        }
+
+        public static Result Compensate<T>(this Result<T> result, Func<string, Result> func)
+        {
+            if (result.IsSuccess)
+            {
+                return Result.Success();
+            }
+
+            return func(result.Error);
+        }
+
+        public static Result<T> Compensate<T>(this Result<T> result, Func<string, Result<T>> func)
+        {
+            if (result.IsSuccess)
+            {
+                return Result.Success(result.Value);
+            }
+
+            return func(result.Error);
+        }
+
+        public static Result<T, E> Compensate<T, E>(this Result<T> result, Func<string, Result<T, E>> func)
+        {
+            if (result.IsSuccess)
+            {
+                return Result.Success<T, E>(result.Value);
+            }
+
+            return func(result.Error);
+        }
+
+        public static Result Compensate<E>(this UnitResult<E> result, Func<E, Result> func)
+        {
+            if (result.IsSuccess)
+            {
+                return Result.Success();
+            }
+
+            return func(result.Error);
+        }
+
+        public static UnitResult<E2> Compensate<E, E2>(this UnitResult<E> result, Func<E, UnitResult<E2>> func)
+        {
+            if (result.IsSuccess)
+            {
+                return UnitResult.Success<E2>();
+            }
+
+            return func(result.Error);
+        }
+
+        public static Result Compensate<T, E>(this Result<T, E> result, Func<E, Result> func)
+        {
+            if (result.IsSuccess)
+            {
+                return Result.Success();
+            }
+
+            return func(result.Error);
+        }
+
+        public static UnitResult<E2> Compensate<T, E, E2>(this Result<T, E> result, Func<E, UnitResult<E2>> func)
+        {
+            if (result.IsSuccess)
+            {
+                return UnitResult.Success<E2>();
+            }
+
+            return func(result.Error);
+        }
+
+        public static Result<T, E2> Compensate<T, E, E2>(this Result<T, E> result, Func<E, Result<T, E2>> func)
+        {
+            if (result.IsSuccess)
+            {
+                return Result.Success<T, E2>(result.Value);
+            }
+
+            return func(result.Error);
+        }
+    }
+}
