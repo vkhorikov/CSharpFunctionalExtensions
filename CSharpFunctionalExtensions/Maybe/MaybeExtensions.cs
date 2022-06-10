d2bbca13981556b20fa9ec581a030bc70abd6cf7
Alexandre Murari Junior <murarialex@outlook.com>
2022-06-10 11:55:23 -0300

    Avoid multiple enumeration
    Fixes #413

--- CSharpFunctionalExtensions/Maybe/MaybeExtensions.cs
+++ CSharpFunctionalExtensions/Maybe/MaybeExtensions.cs
@@ -329,6 +329,8 @@ namespace CSharpFunctionalExtensions
 
         public static Maybe<T> TryFirst<T>(this IEnumerable<T> source)
         {
+            source = source as ICollection<T> ?? source.ToList();
+            
             if (source.Any())
             {
                 return Maybe<T>.From(source.First());
@@ -350,6 +352,8 @@ namespace CSharpFunctionalExtensions
 
         public static Maybe<T> TryLast<T>(this IEnumerable<T> source)
         {
+            source = source as ICollection<T> ?? source.ToList();
+            
             if (source.Any())
             {
                 return Maybe<T>.From(source.Last());
