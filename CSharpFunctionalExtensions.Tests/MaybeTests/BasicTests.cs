747f1b43e930d70af85520f19df16e243ce8cc8c
Prophet Lamb <prophet.lamb@gmail.com>
2022-06-28 17:03:08 +0200

    Nullable and default conversion tests

--- CSharpFunctionalExtensions.Tests/MaybeTests/BasicTests.cs
+++ CSharpFunctionalExtensions.Tests/MaybeTests/BasicTests.cs
@@ -172,6 +172,30 @@ namespace CSharpFunctionalExtensions.Tests.MaybeTests
             
             act.Should().NotThrow();
         }
+        
+        public void Maybe_struct_default_is_none()
+        {
+            Maybe<int> maybe = default;
+
+            maybe.HasValue.Should().BeFalse();
+            maybe.HasNoValue.Should().BeTrue();
+        }
+        
+        public void Maybe_struct_value_is_some()
+        {
+            Maybe<int> maybe = 5;
+
+            maybe.HasValue.Should().BeTrue();
+            maybe.HasNoValue.Should().BeFalse();
+        }
+        
+        public void Maybe_class_null_is_none()
+        {
+            Maybe<MyClass> maybe = null;
+
+            maybe.HasValue.Should().BeFalse();
+            maybe.HasNoValue.Should().BeTrue();
+        }
 
         private class MyClass
         {
