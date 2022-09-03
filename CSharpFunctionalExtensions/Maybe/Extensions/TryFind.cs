9f99d740f7e5d35ee199f02f2e1497e8e8670303
T.M <tomasz.piotr.malinowski@gmail.com>
2022-09-03 22:31:57 +0200

    fix IReadOnlyDictionary.TryFind() availability on .NET 6

--- CSharpFunctionalExtensions/Maybe/Extensions/TryFind.cs
+++ CSharpFunctionalExtensions/Maybe/Extensions/TryFind.cs
@@ -4,7 +4,7 @@ namespace CSharpFunctionalExtensions
 {
     public static partial class MaybeExtensions
     {
-#if !NETCORE || !NET45_OR_GREATER || !NETSTANDARD
+#if !NETCORE && !NET45_OR_GREATER && !NETSTANDARD && !NET
         public static Maybe<V> TryFind<K, V>(this IDictionary<K, V> dict, K key)
         {
             if (dict.ContainsKey(key)) return dict[key];
