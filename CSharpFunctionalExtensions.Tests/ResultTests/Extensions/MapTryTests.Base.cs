2ebadb8bb41d05aebd21b2db337c22f323b1764c
rutkowskit <rutkowskit@gmail.com>
2022-09-16 08:32:09 +0200

    Add MapTry Base extensions. Close #12

---
+++ CSharpFunctionalExtensions.Tests/ResultTests/Extensions/MapTryTests.Base.cs
@@ -0,0 +1,82 @@
+﻿using CSharpFunctionalExtensions.Tests.MaybeTests.Extensions;
+using FluentAssertions;
+using System;
+using System.Threading.Tasks;
+
+namespace CSharpFunctionalExtensions.Tests.ResultTests.Extensions
+{
+    public class MapTryTestsBase : MapTestsBase
+    {        
+        protected K Throwing_K() => throw new Exception(ErrorMessage, GetFuncException());
+        protected K Throwing_T_K(T _) => throw new Exception(ErrorMessage, GetFuncException());
+
+        protected Task<K> Task_Throwing_K() => throw new Exception(ErrorMessage, GetFuncException());
+        protected Task<K> Task_Throwing_T_K(T _) => throw new Exception(ErrorMessage, GetFuncException());
+
+        protected ValueTask<K> ValueTask_Throwing_K() => throw new Exception(ErrorMessage, GetFuncException());
+        protected ValueTask<K> ValueTask_Throwing_T_K(T _) => throw new Exception(ErrorMessage, GetFuncException());
+
+
+        protected static string ErrorHandler(Exception _) => ErrorMessage2;
+        protected static E ErrorHandler_E(Exception _) => E.Value2;
+
+        protected void AssertSuccess(Result<K,E> result)
+        {
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().Be(K.Value);
+            FuncExecuted.Should().BeTrue();
+        }
+
+        protected void AssertSuccess(Result<K> result)
+        {
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().Be(K.Value);                   
+            FuncExecuted.Should().BeTrue();
+        }
+
+        protected void AssertFailure(Result<K,E> result)
+        {
+            AssertFailure(result, E.Value, false);
+        }
+        protected void AssertFailureFromHandler(Result<K, E> result)
+        {
+            AssertFailure(result, E.Value2, true);
+        }
+        protected void AssertFailure(Result result)
+        {
+            AssertFailure(result, ErrorMessage, false);
+        }
+        protected void AssertFailureFromHandler(Result result)
+        {
+            AssertFailure(result, ErrorMessage2, true);
+        }
+        protected void AssertFailureFromDefaultHandler(Result result)
+        {
+            AssertFailure(result, ErrorMessage, true);
+        }
+
+        protected void AssertFailure(Result result, string message, bool fromFunc)
+        {
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(message);
+            FuncExecuted.Should().Be(fromFunc);
+        }
+        protected void AssertFailure(Result<T,E> result, E error, bool fromFunc)
+        {
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(error);
+            FuncExecuted.Should().Be(fromFunc);
+        }
+        protected void AssertFailure(UnitResult<E> result, E errorValue, bool fromFunc)
+        {
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().Be(errorValue);
+            FuncExecuted.Should().Be(fromFunc);
+        }
+        private Exception GetFuncException()
+        {
+            Func_K();
+            return new Exception("Thrown from function");
+        }
+    }
+}
---
+++ CSharpFunctionalExtensions/Result/Methods/Extensions/MapTry.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CSharpFunctionalExtensions
+{
+    public static partial class ResultExtensions
+    {
+        /// <summary>
+        ///     Creates a new result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+        ///     If a given function throws an exception, an error is returned from the given error handler
+        /// </summary>
+        public static Result<K, E> MapTry<T, K, E>(this Result<T, E> result, Func<T, K> func,
+            Func<Exception, E> errorHandler)
+        {
+            return result.IsFailure
+                ? Result.Failure<K, E>(result.Error)
+                : Result.Try(() => func(result.Value), errorHandler);  
+        }
+
+        /// <summary>
+        ///     Creates a new result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+        ///     If a given function throws an exception, an error is returned from the given error handler
+        /// </summary>
+        public static Result<K, E> MapTry<K, E>(this UnitResult<E> result, Func<K> func,
+            Func<Exception, E> errorHandler)
+        {
+            return result.IsFailure
+                ? Result.Failure<K, E>(result.Error)
+                : Result.Try(() => func(), errorHandler);
+        }
+
+        /// <summary>
+        ///     Creates a new result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+        ///     If a given function throws an exception, an error is returned from the given error handler
+        /// </summary>
+        public static Result<K> MapTry<T, K>(this Result<T> result, Func<T, K> func,
+            Func<Exception, string> errorHandler = null)
+        {
+            return result.IsFailure
+                ? Result.Failure<K>(result.Error)
+                : Result.Try(() => func(result.Value), errorHandler);
+        }
+
+        /// <summary>
+        ///     Creates a new result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+        ///     If a given function throws an exception, an error is returned from the given error handler
+        /// </summary>
+        public static Result<K> MapTry<K>(this Result result, Func<K> func,
+            Func<Exception, string> errorHandler = null)
+        {
+            return result.IsFailure
+                ? Result.Failure<K>(result.Error)
+                : Result.Try(() => func(), errorHandler);
+        }
+    }
+}
