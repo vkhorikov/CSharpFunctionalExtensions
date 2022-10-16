e0806ae5e8262bc8b98e465a3d06ef3090a89747
max arshinov <max.arshinov@gmail.com>
2022-10-16 23:14:49 +0200

    TId

--- CSharpFunctionalExtensions/AggregateRoot.cs
+++ CSharpFunctionalExtensions/AggregateRoot.cs
@@ -7,7 +7,7 @@ namespace CSharpFunctionalExtensions
         IEnumerable<TDomainEvent> DomainEvents { get; }
     }
 
-    public abstract class AggregateRoot<TDomainEvent> : Entity, IAggregateRoot<TDomainEvent>
+    public abstract class AggregateRoot<TId, TDomainEvent> : Entity<TId>, IAggregateRoot<TDomainEvent>
     {
         private readonly List<TDomainEvent> _domainEvents = new List<TDomainEvent>();
         public virtual IEnumerable<TDomainEvent> DomainEvents => _domainEvents;
