9fa17f260a9e50b69091acaa616de90545e8d58f
sviataslau.hankovich <sviataslau.hankovich@fabros-team.com>
2022-10-28 11:59:50 +0300

    fix build

--- CSharpFunctionalExtensions/Result/Methods/Extensions/EnsureNotNull.Task.Left.cs
+++ CSharpFunctionalExtensions/Result/Methods/Extensions/EnsureNotNull.Task.Left.cs
@@ -3,6 +3,12 @@
 using System;
 using System.Threading.Tasks;
 
+#if NET40
+using Task = System.Threading.Tasks.TaskEx;
+#else
+using Task = System.Threading.Tasks.Task;
+#endif
+
 namespace CSharpFunctionalExtensions
 {
     public static partial class ResultExtensions
