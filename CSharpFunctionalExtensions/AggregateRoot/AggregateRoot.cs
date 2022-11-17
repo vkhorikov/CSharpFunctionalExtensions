b96dd4186d0ea398014709398fe3c091cccc857e
Xavier John <xavierjohn@hotmail.com>
2022-11-16 18:35:24 -0800

    Add AggregateRoot<TId>

---
+++ CSharpFunctionalExtensions/AggregateRoot/AggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpFunctionalExtensions
+{
+    public abstract class AggregateRoot<TId> : Entity<TId>
+        where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id)
+        {
+        }
+    }
+}
