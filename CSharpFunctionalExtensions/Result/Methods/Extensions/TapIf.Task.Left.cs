19df0b0ce1ce4b5e036695ad539b12e925dafc0d
Scheichsbeutel <Felix.Liebrecht@t-online.de>
2023-09-26 11:39:56 +0200

    Update TapIf.Task.Left.cs
    This commit corresponds to Issue #515

--- CSharpFunctionalExtensions/Result/Methods/Extensions/TapIf.Task.Left.cs
+++ CSharpFunctionalExtensions/Result/Methods/Extensions/TapIf.Task.Left.cs
@@ -74,6 +74,19 @@ namespace CSharpFunctionalExtensions
         /// <summary>
         ///     Executes the given action if the calling result is a success and condition is true. Returns the calling result.
         /// </summary>
+        public static async Task<Result> TapIf(this Task<Result> resultTask, Func<bool> predicate, Action action)
+        {
+            Result result = await resultTask.DefaultAwait();
+
+            if (result.IsSuccess && predicate())
+                return result.Tap(action);
+            else
+                return result;
+        }
+
+        /// <summary>
+        ///     Executes the given action if the calling result is a success and condition is true. Returns the calling result.
+        /// </summary>
         public static async Task<Result<T>> TapIf<T>(this Task<Result<T>> resultTask, Func<T, bool> predicate, Action action)
         {
             Result<T> result = await resultTask.DefaultAwait();
