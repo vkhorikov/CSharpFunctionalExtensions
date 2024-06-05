47949789a103d3cb2374cf10233a29bc6773f41d
Julien Aspirot <julien.aspirot@usherbrooke.ca>
2024-06-05 19:30:35 -0400

    add ToUnitResult extension for Maybe, in two flavors, for sync and async usage

---
+++ CSharpFunctionalExtensions/Maybe/Extensions/ToUnitResult.Task.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace CSharpFunctionalExtensions
+{
+    public static partial class MaybeExtensions
+    {
+        public static async Task<UnitResult<E>> ToUnitResult<E>(this Task<Maybe<E>> maybeTask)
+        {
+            var maybe = await maybeTask.DefaultAwait();
+            return maybe.ToUnitResult();
+        }
+
+        public static async Task<UnitResult<E>> ToUnitResult<T, E>(this Task<Maybe<T>> maybeTask, E error)
+        {
+            var maybe = await maybeTask.DefaultAwait();
+            return maybe.ToUnitResult(error);
+        }
+    }
+}
---
+++ CSharpFunctionalExtensions/Maybe/Extensions/ToUnitResult.ValueTask.cs
@@ -0,0 +1,22 @@
+﻿#if NET5_0_OR_GREATER
+using System;
+using System.Threading.Tasks;
+
+namespace CSharpFunctionalExtensions.ValueTasks
+{
+    public static partial class MaybeExtensions
+    {
+        public static async ValueTask<UnitResult<E>> ToUnitResult<E>(this ValueTask<Maybe<E>> maybeTask)
+        {
+            Maybe<E> maybe = await maybeTask;
+            return maybe.ToUnitResult();
+        }
+
+        public static async ValueTask<UnitResult<E>> ToUnitResult<T, E>(this ValueTask<Maybe<T>> maybeTask, E error)
+        {
+            Maybe<T> maybe = await maybeTask;
+            return maybe.ToUnitResult(error);
+        }
+    }
+}
+#endif
