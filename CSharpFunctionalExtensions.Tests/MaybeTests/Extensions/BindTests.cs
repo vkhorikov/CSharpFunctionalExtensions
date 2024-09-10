e7e03e5dc8bb23eef9c0900e444a28b2d709d37d
Marcin Jahn <10273406+marcinjahn@users.noreply.github.com>
2024-09-10 21:07:48 +0200

    Add allocation-free Maybe.Bind overload

--- CSharpFunctionalExtensions.Tests/MaybeTests/Extensions/BindTests.cs
+++ CSharpFunctionalExtensions.Tests/MaybeTests/Extensions/BindTests.cs
@@ -35,5 +35,63 @@ namespace CSharpFunctionalExtensions.Tests.MaybeTests.Extensions
             maybe2.HasValue.Should().BeTrue();
             maybe2.Value.Should().Be(T.Value);
         }
+
+        [Fact]
+        public void Bind_provides_context_to_selector()
+        {
+            Maybe<T> maybe = null;
+            var context = 5;
+
+            var maybe2 = maybe.Bind(
+                (value, ctx) =>
+                {
+                    ctx.Should().Be(context);
+                    return Maybe.From(value);
+                },
+                context
+            );
+
+            maybe2.HasValue.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Bind_with_context_returns_no_value_if_initial_maybe_is_null()
+        {
+            Maybe<T> maybe = null;
+
+            var maybe2 = maybe.Bind(
+                (value, _) => ExpectAndReturnMaybe(null, T.Value)(value),
+                context: 5
+            );
+
+            maybe2.HasValue.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Bind_with_context_returns_no_value_if_selector_returns_null()
+        {
+            Maybe<T> maybe = T.Value;
+
+            var maybe2 = maybe.Bind(
+                (value, _) => ExpectAndReturn(T.Value, Maybe<T>.None)(value),
+                context: 5
+            );
+
+            maybe2.HasValue.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Bind_with_context_returns_value_if_selector_returns_value()
+        {
+            Maybe<T> maybe = T.Value;
+
+            var maybe2 = maybe.Bind(
+                (value, _) => ExpectAndReturnMaybe<T>(T.Value, T.Value)(value),
+                5
+            );
+
+            maybe2.HasValue.Should().BeTrue();
+            maybe2.Value.Should().Be(T.Value);
+        }
     }
 }
--- CSharpFunctionalExtensions/Maybe/Extensions/Bind.cs
+++ CSharpFunctionalExtensions/Maybe/Extensions/Bind.cs
@@ -11,5 +11,17 @@ namespace CSharpFunctionalExtensions
 
             return selector(maybe.GetValueOrThrow());
         }
+
+        public static Maybe<K> Bind<T, K, TContext>(
+            in this Maybe<T> maybe,
+            Func<T, TContext, Maybe<K>> selector,
+            TContext context
+        )
+        {
+            if (maybe.HasNoValue)
+                return Maybe<K>.None;
+
+            return selector(maybe.GetValueOrThrow(), context);
+        }
     }
 }
