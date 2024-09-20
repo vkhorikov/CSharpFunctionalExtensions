9a80bf2c1e5f9314c85dd7274e117e17f5846f2e
Christopher Calmes <christopher.calmes@pacesetterclaims.com>
2024-09-20 14:35:55 -0500

    added select overload with projection for result

---
+++ CSharpFunctionalExtensions/Result/Methods/Extensions/Select.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpFunctionalExtensions
+{
+    public static partial class ResultExtensions
+    {
+        /// <summary>
+        ///     This method should be used in linq queries. We recommend using Map method.
+        /// </summary>
+        public static Result<K> Select<T, K>(in this Result<T> result, Func<T, K> selector)
+        {
+            return result.Map(selector);
+        }
+    }
+}
