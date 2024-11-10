7d871860fc1954de9e135549ac55fc493f9b19f8
Marcin Jahn <10273406+marcinjahn@users.noreply.github.com>
2024-11-10 15:58:46 +0100

    feat: add allocation-free overloads for Result.MapIf

--- CSharpFunctionalExtensions.Tests/ResultTests/Extensions/MapIfTests.cs
+++ CSharpFunctionalExtensions.Tests/ResultTests/Extensions/MapIfTests.cs
@@ -10,12 +10,13 @@ namespace CSharpFunctionalExtensions.Tests.ResultTests.Extensions
         [InlineData(true, false)]
         [InlineData(false, true)]
         [InlineData(false, false)]
-        public void MapIf_T_executes_func_conditionally_and_returns_new_result(bool isSuccess, bool condition)
+        public void MapIf_T_executes_func_conditionally_and_returns_new_result(
+            bool isSuccess,
+            bool condition
+        )
         {
             Result<T> result = Result.SuccessIf(isSuccess, T.Value, ErrorMessage);
-
             Result<T> returned = result.MapIf(condition, GetAction());
-
             actionExecuted.Should().Be(isSuccess && condition);
             returned.Should().Be(GetExpectedValueResult(isSuccess, condition));
         }
@@ -25,12 +26,13 @@ namespace CSharpFunctionalExtensions.Tests.ResultTests.Extensions
         [InlineData(true, false)]
         [InlineData(false, true)]
         [InlineData(false, false)]
-        public void MapIf_T_E_executes_func_conditionally_and_returns_new_result(bool isSuccess, bool condition)
+        public void MapIf_T_E_executes_func_conditionally_and_returns_new_result(
+            bool isSuccess,
+            bool condition
+        )
         {
             Result<T, E> result = Result.SuccessIf(isSuccess, T.Value, E.Value);
-
             Result<T, E> returned = result.MapIf(condition, GetAction());
-
             actionExecuted.Should().Be(isSuccess && condition);
             returned.Should().Be(GetExpectedValueErrorResult(isSuccess, condition));
         }
@@ -40,12 +42,13 @@ namespace CSharpFunctionalExtensions.Tests.ResultTests.Extensions
         [InlineData(true, false)]
         [InlineData(false, true)]
         [InlineData(false, false)]
-        public void MapIf_computes_predicate_T_executes_func_conditionally_and_returns_new_result(bool isSuccess, bool condition)
+        public void MapIf_computes_predicate_T_executes_func_conditionally_and_returns_new_result(
+            bool isSuccess,
+            bool condition
+        )
         {
             Result<T> result = Result.SuccessIf(isSuccess, T.Value, ErrorMessage);
-
             Result<T> returned = result.MapIf(GetValuePredicate(condition), GetAction());
-
             predicateExecuted.Should().Be(isSuccess);
             actionExecuted.Should().Be(isSuccess && condition);
             returned.Should().Be(GetExpectedValueResult(isSuccess, condition));
@@ -56,12 +59,119 @@ namespace CSharpFunctionalExtensions.Tests.ResultTests.Extensions
         [InlineData(true, false)]
         [InlineData(false, true)]
         [InlineData(false, false)]
-        public void MapIf_computes_predicate_T_E_executes_func_conditionally_and_returns_new_result(bool isSuccess, bool condition)
+        public void MapIf_computes_predicate_T_E_executes_func_conditionally_and_returns_new_result(
+            bool isSuccess,
+            bool condition
+        )
         {
             Result<T, E> result = Result.SuccessIf(isSuccess, T.Value, E.Value);
-
             Result<T, E> returned = result.MapIf(GetValuePredicate(condition), GetAction());
+            predicateExecuted.Should().Be(isSuccess);
+            actionExecuted.Should().Be(isSuccess && condition);
+            returned.Should().Be(GetExpectedValueErrorResult(isSuccess, condition));
+        }
 
+        [Theory]
+        [InlineData(true, true)]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        [InlineData(false, false)]
+        public void MapIf_T_with_context_executes_func_conditionally_and_passes_context(
+            bool isSuccess,
+            bool condition
+        )
+        {
+            Result<T> result = Result.SuccessIf(isSuccess, T.Value, ErrorMessage);
+            Result<T> returned = result.MapIf(
+                condition,
+                (value, context) =>
+                {
+                    context.Should().Be(ContextMessage);
+                    return GetAction()(value);
+                },
+                ContextMessage
+            );
+            actionExecuted.Should().Be(isSuccess && condition);
+            returned.Should().Be(GetExpectedValueResult(isSuccess, condition));
+        }
+
+        [Theory]
+        [InlineData(true, true)]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        [InlineData(false, false)]
+        public void MapIf_T_E_with_context_executes_func_conditionally_and_passes_context(
+            bool isSuccess,
+            bool condition
+        )
+        {
+            Result<T, E> result = Result.SuccessIf(isSuccess, T.Value, E.Value);
+            Result<T, E> returned = result.MapIf(
+                condition,
+                (value, context) =>
+                {
+                    context.Should().Be(ContextMessage);
+                    return GetAction()(value);
+                },
+                ContextMessage
+            );
+            actionExecuted.Should().Be(isSuccess && condition);
+            returned.Should().Be(GetExpectedValueErrorResult(isSuccess, condition));
+        }
+
+        [Theory]
+        [InlineData(true, true)]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        [InlineData(false, false)]
+        public void MapIf_computes_predicate_T_with_context_executes_func_conditionally_and_passes_context(
+            bool isSuccess,
+            bool condition
+        )
+        {
+            Result<T> result = Result.SuccessIf(isSuccess, T.Value, ErrorMessage);
+            Result<T> returned = result.MapIf(
+                (value, context) =>
+                {
+                    context.Should().Be(ContextMessage);
+                    return GetValuePredicate(condition)(value);
+                },
+                (value, context) =>
+                {
+                    context.Should().Be(ContextMessage);
+                    return GetAction()(value);
+                },
+                ContextMessage
+            );
+            predicateExecuted.Should().Be(isSuccess);
+            actionExecuted.Should().Be(isSuccess && condition);
+            returned.Should().Be(GetExpectedValueResult(isSuccess, condition));
+        }
+
+        [Theory]
+        [InlineData(true, true)]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        [InlineData(false, false)]
+        public void MapIf_computes_predicate_T_E_with_context_executes_func_conditionally_and_passes_context(
+            bool isSuccess,
+            bool condition
+        )
+        {
+            Result<T, E> result = Result.SuccessIf(isSuccess, T.Value, E.Value);
+            Result<T, E> returned = result.MapIf(
+                (value, context) =>
+                {
+                    context.Should().Be(ContextMessage);
+                    return GetValuePredicate(condition)(value);
+                },
+                (value, context) =>
+                {
+                    context.Should().Be(ContextMessage);
+                    return GetAction()(value);
+                },
+                ContextMessage
+            );
             predicateExecuted.Should().Be(isSuccess);
             actionExecuted.Should().Be(isSuccess && condition);
             returned.Should().Be(GetExpectedValueErrorResult(isSuccess, condition));
