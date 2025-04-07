cab3217ca130cf59165cc16354b1c63d057fc2f4
Yehuda Ringler <yehuda.ringler@chabad.org>
2025-04-07 14:57:52 -0400

    test bindoptional e

--- CSharpFunctionalExtensions.Tests/ResultTests/Extensions/BindOptionalTests.cs
+++ CSharpFunctionalExtensions.Tests/ResultTests/Extensions/BindOptionalTests.cs
@@ -9,6 +9,44 @@ public class BindOptionalTests
     [Fact]
     public void BindOptional_returns_result_value()
     {
+        var present = Result.Success<Maybe<string>>("yay")
+            .BindOptional(x => Result.Success<int>(1));
+
+        present.Value.Should().Be(1);
+    }
+
+    [Fact]
+    public void BindOptional_with_nothing_is_success()
+    {
+        var absent  = Result.Success<Maybe<string>>(Maybe<string>.None)
+            .BindOptional(x => Result.Success<int>(1));
+        absent.IsSuccess.Should().BeTrue();
+        absent.Value.HasNoValue.Should().BeTrue();
+    }
+
+    [Fact]
+    public void BindOptional_initial_failure_bind_not_run()
+    {
+        var initialFailure = Result.Failure<Maybe<string>>("nay")
+            .BindOptional(x => Result.Success<int>(1));
+
+        initialFailure.IsFailure.Should().BeTrue();
+        initialFailure.Error.IsSameOrEqualTo("nay");
+    }
+    
+    [Fact]
+    public void BindOptional_bind_fails_is_error()
+    {
+        var bindFailure = Result.Success<Maybe<string>>("yay")
+            .BindOptional(x => Result.Failure<string>("nay"));
+
+        bindFailure.IsFailure.Should().BeTrue();
+        bindFailure.Error.IsSameOrEqualTo("nay");
+    }
+    
+    [Fact]
+    public void BindOptional_E_returns_result_value()
+    {
         var present = Result.Success<Maybe<string>, string>("yay")
             .BindOptional(x => Result.Success<int, string>(1));
 
@@ -16,7 +54,7 @@ public class BindOptionalTests
     }
 
     [Fact]
-    public void BindOptional_with_nothing_is_success()
+    public void BindOptional_E_with_nothing_is_success()
     {
         var absent  = Result.Success<Maybe<string>, string>(Maybe<string>.None)
             .BindOptional(x => Result.Success<int, string>(1));
@@ -25,7 +63,7 @@ public class BindOptionalTests
     }
 
     [Fact]
-    public void BindOptional_initial_failure_bind_not_run()
+    public void BindOptional_E_initial_failure_bind_not_run()
     {
         var initialFailure = Result.Failure<Maybe<string>, string>("nay")
             .BindOptional(x => Result.Success<int, string>(1));
@@ -35,7 +73,7 @@ public class BindOptionalTests
     }
     
     [Fact]
-    public void BindOptional_bind_fails_is_error()
+    public void BindOptional_E_bind_fails_is_error()
     {
         var bindFailure = Result.Success<Maybe<string>, string>("yay")
             .BindOptional(x => Result.Failure<string, string>("nay"));
