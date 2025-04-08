080e6a232d6ee28e556a5ff3714b1a04e025bdbb
Yehuda Ringler <yehuda.ringler@chabad.org>
2025-04-08 12:11:01 -0400

    test ensure not

--- CSharpFunctionalExtensions.Tests/ResultTests/Extensions/EnsureNotTests.cs
+++ CSharpFunctionalExtensions.Tests/ResultTests/Extensions/EnsureNotTests.cs
@@ -1,18 +1,48 @@
+using FluentAssertions;
 using Xunit;
 
-namespace CSharpFunctionalExtensions.Tests.ResultTests.Extensions;
-
-public class EnsureNotTests
+namespace CSharpFunctionalExtensions.Tests.ResultTests.Extensions
 {
-    [Fact]
-    public void EnsureNot_passes_when_test_fails()
+    public class EnsureNotTests
     {
-        
-    }
+        [Fact]
+        public void EnsureNot_passes_when_test_fails()
+        {
+            var result = Result.Success(5);
+            var newResult = result.EnsureNot(x => x > 10, "Value should be less than or equal to 10");
 
-    [Fact]
-    public void EnsureNot_Fails_with_error_when_test_passes()
-    {
-        
+            newResult.IsSuccess.Should().BeTrue();
+            newResult.Value.Should().Be(5);
+        }
+
+        [Fact]
+        public void EnsureNot_Fails_with_error_when_test_passes()
+        {
+            var result = Result.Success(15);
+            var newResult = result.EnsureNot(x => x > 10, "Value should be less than or equal to 10");
+
+            newResult.IsFailure.Should().BeTrue();
+            newResult.Error.Should().Be("Value should be less than or equal to 10");
+        }
+
+        [Fact]
+        public void EnsureNot_E_passes_when_test_fails()
+        {
+            var result = Result.Success<int, string>(5);
+            var newResult = result.EnsureNot(x => x > 10, "Value should be less than or equal to 10");
+
+            newResult.IsSuccess.Should().BeTrue();
+            newResult.Value.Should().Be(5);
+        }
+
+        [Fact]
+        public void EnsureNot_E_Fails_with_error_when_test_passes()
+        {
+            var result = Result.Success<int, string>(15);
+            var newResult = result.EnsureNot(x => x > 10, "Value should be less than or equal to 10");
+
+            newResult.IsFailure.Should().BeTrue();
+            newResult.Error.Should().Be("Value should be less than or equal to 10");
+        }
     }
 }
